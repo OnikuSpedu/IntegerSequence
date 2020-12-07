495f01548b60c36c2a096da2b4e39a07f7c44878
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-07 13:33:09 -0500

    Created a tester

---
+++ Tester.java
@@ -0,0 +1,19 @@
+public class Tester {
+    public static void main(String[] args) {
+        IntegerSequence r = new Range(10,20);
+        System.out.println("Length: " + r.length());
+        while(r.hasNext()){
+        System.out.print(r.next());
+        if( r.hasNext() )       
+            System.out.print( ", " );
+        }
+        System.out.println();
+        r.reset();
+        while(r.hasNext()){
+            System.out.print(r.next());
+            if( r.hasNext() )       
+                System.out.print( ", " );
+            }
+        System.out.println();
+    }
+}
