3f7ac7d9202c028044c039aba21855c445230569
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-07 13:32:44 -0500

    Created Range class which implements IntegerSequence

---
+++ Range.java
@@ -0,0 +1,28 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+    private int start,end,current;
+    public Range(int start,  int end){  
+        this.start = start;
+        this.end = end;
+        this.current = start;
+    }
+    public void reset(){
+        this.current = this.start;
+    }
+    public int length(){  
+        return this.end - this.start + 1;
+    }
+    public boolean hasNext(){ 
+        return this.current <= this.end;
+    }
+
+    //@throws NoSuchElementException
+    public int next() {
+        if (hasNext()) {
+            int cur = this.current;
+            this.current++;
+            return cur;
+        }
+        else throw new NoSuchElementException();
+    }
+}
