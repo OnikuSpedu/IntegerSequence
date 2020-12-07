db9878ad1bea6239215f0130ee9e3fd05fa8b823
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-07 17:32:07 -0500

    More tests

--- Tester.java
+++ Tester.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class Tester {
     public static void main(String[] args) {
         IntegerSequence r = new Range(10,20);
@@ -15,6 +17,15 @@ public class Tester {
                 System.out.print( ", " );
             }
         System.out.println();
-        System.out.print(r.next());
+        try {
+            System.out.print(r.next());
+        } catch(NoSuchElementException e){
+            e.printStackTrace();
+        }
+        try {
+            IntegerSequence illegalRange = new Range(100,20);
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
     }
 }
