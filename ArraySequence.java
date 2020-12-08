56502ba85f442bfb3fd4e5a483caa5ab01fb510b
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-08 12:47:57 -0500

    Added required interface elements and constructor.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,33 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
-  private int currentIndex;
-  private int []data;
+    private int currentIndex;
+    private int []data;
 
-  /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+    public boolean hasNext() {
+        return this.currentIndex >= 0 && this.currentIndex <= length();
+    }
+
+    public int length() {
+        return data.length;
+    }
+
+    public void reset() {
+        this.currentIndex =  0;    
+    }
+
+    public int next() {
+        if (hasNext()) {
+            int cur = this.currentIndex;
+            this.currentIndex++;
+            return cur;
+        }
+        else throw new NoSuchElementException();
+    }
+    /*Construct the sequence by copying values from the other array into the data array*/
+    public ArraySequence(int [] other){  
+        int[] data = new int[other.length];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = other[i];
+        }
+    }
 }
